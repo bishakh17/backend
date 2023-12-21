cd93a0d2dde02650f2a75485d8e6a2b9bc244939
bishakh17 <telbdzone@gmail.com>
2023-12-21 15:16:46 +0530

    initial

---
+++ javascript/variables.js
@@ -0,0 +1,10 @@
+//let variables can be changed and do not need to be initialized
+let age = 21;
+let result;
+
+//all const variables must be initialized and cannot be changed
+const salary = 1000;
+
+console.log(age);
+console.log(salary);
+console.log(result);
